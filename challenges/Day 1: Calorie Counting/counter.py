fe5f44b41bb999536568e606fb0f7fbcc7bc6b8c
Federico Lochbaum <federico.lochbaum@gmail.com>
2022-12-01 14:13:38 +0100

    Day 1

---
+++ challenges/Day 1: Calorie Counting/counter.py
@@ -0,0 +1,37 @@
+## Get the maximum number of calories ( trivial )
+input_path = 'challenges/Day 1: Calorie Counting/input'
+
+maximum = 0
+current_mount = 0
+
+## Part 1
+## O(n), just getting the greatest value
+with open(input_path) as f:
+  for line in f:
+    if (line == '\n'):
+      maximum = max(maximum, current_mount)
+      current_mount = 0
+      continue
+    current_mount += int(line)
+
+print('Part 1: ', maximum)
+
+## Part 2
+## O(n), how the input is too small,
+## I only need to sort the array, which is lineal but is enough good
+## Other optio is to use a priority fixed buffer (where we could void sorting a lineal array)
+
+list = []
+current_mount_2 = 0
+
+with open(input_path) as f:
+  for line in f:
+    if (line == '\n'):
+      list.append(current_mount_2)
+      current_mount_2 = 0
+      continue
+    current_mount_2 += int(line)
+
+list.sort()
+
+print('Part 2: ', sum(list[-3:]))
