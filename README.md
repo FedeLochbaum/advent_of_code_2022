75139492711e9af3e9e681e75f665f0453fe2567
Federico Lochbaum <federico.lochbaum@gmail.com>
2022-12-03 07:06:26 +0100

    Day 3

--- README.md
+++ README.md
@@ -7,3 +7,5 @@
 [Day 1: Calorie Counting](https://github.com/FedeLochbaum/advent_of_code_2022/blob/main/challenges/Day%201:%20Calorie%20Counting/counter.py)
 
 [Day 2: Rock Paper Scissors](https://github.com/FedeLochbaum/advent_of_code_2022/blob/main/challenges/Day%202:%20Rock%20Paper%20Scissors/scoring.py)
+
+[Day 3: Rucksack Reorganization](https://github.com/FedeLochbaum/advent_of_code_2022/blob/main/challenges/Day%203:%20Rucksack%20Reorganization/scoring.py)
---
+++ challenges/Day 3: Rucksack Reorganization/rearranging.py
@@ -0,0 +1,36 @@
+import string
+input_path = 'advent_of_code_2022/challenges/Day 3: Rucksack Reorganization/input'
+
+# Part 1
+NUM = 31
+
+def value(char): return (ord(char) & NUM) + (26 if char.isupper() else 0)
+
+count = 0
+
+with open(input_path) as f:
+  for line in f:
+    c1 = line[:int(line.__len__() / 2)]
+    c2 = line[int(line.__len__() / 2):]
+    for c in c2:
+      if(c in c1):
+        count += value(c)
+        break
+
+print('Part 1:', count)
+
+# Part 2
+group = []
+count = 0
+
+with open(input_path) as f:
+  for line in f:
+    group.append(line)
+    if (group.__len__() == 3):
+      for c in group[0]:
+        if (c in group[1] and c in group[2]):
+          count += value(c)
+          break
+      group = []
+
+print('Part 2:', count)
