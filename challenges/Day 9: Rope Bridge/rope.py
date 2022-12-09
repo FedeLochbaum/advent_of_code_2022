ac432b94c87b93748a267efd7e26d8ac9f7e8395
Federico Lochbaum <federico.lochbaum@gmail.com>
2022-12-09 09:59:24 +0000

    day 9, doesn't work yet

--- challenges/Day 9: Rope Bridge/rope.py
+++ challenges/Day 9: Rope Bridge/rope.py
@@ -1,55 +1,42 @@
 input_path = 'advent_of_code_2022/challenges/Day 9: Rope Bridge/input'
 
 visited = set()
-head = [4, 0]
-tail = [4, 0]
+head = [0, 0]
+tail = [0, 0]
 
-next_head_by_side = {
+next_head = {
   'R': lambda point: [point[0], point[1] + 1],
   'L': lambda point: [point[0], point[1] - 1],
   'U': lambda point: [point[0] - 1, point[1]],
   'D': lambda point: [point[0] + 1, point[1]]
 }
 
-def distance(p1, p2): return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
-
 def move_diagonal(h, t):
-  if (h[1] < t[1]): return [t[0] - 1, t[1] - 1] if h[0] < t[0] else [t[0] + 1, t[1] -1]
-  
+  if (h[1] < t[1]): return [t[0] - 1, t[1] - 1] if h[0] < t[0] else [t[0] + 1, t[1] - 1]
+
   return [t[0] - 1, t[1] + 1] if h[0] < t[0] else [t[0] + 1, t[1] + 1]
 
-# def move_lineal(h, t)
+def move_lineal(h, t):
+  if (h[0] == t[0]): return [t[0], t[1] - 1] if h[1] < t[1] else [t[0], t[1] + 1]
 
-def next_tail(p1, p2):
-  return (
-    move_diagonal(p1, p2)
-    if (p1[1] != p2[1] and p1[0] != p2[0])
-    else [int((p1[0] + p2[0])/2), int((p1[1] + p2[1])/2)] #move_lineal(p1, p2)
-  )
+  return [t[0] - 1, t[1]] if h[0] < t[0] else [t[0] + 1, t[1]]
 
-def print_p(r, c, h, t):
-  if h == [r, c]: return 'H'
-  if t == [r, c]: return 'T'
-  return '.'
+def next_tail(h, t): return (
+  move_diagonal(h, t)
+  if h[0] != t[0] and h[1] != t[1]
+  else move_lineal(h, t)
+)
 
-def print_points(h, t):
-  for r in range(5):
-    print(' '.join(print_p(r, c, h, t) for c in range(6)))
-  print('\n')
-
-def must_update(p1, p2):
-  return max(abs(p1[0] - p2[0]), abs(p1[1] - p2[1])) > 1
+def should_move(h, t): return abs(h[0] - t[0]) == 2 or abs(h[1] - t[1]) == 2
 
 with open(input_path) as f:
   visited.add(''.join(str(e) for e in tail))
   for line in f:
     side, count = line[:-1].split(' ')
-    print('== ', line[:-1] ,' ==\n')
     for _ in range(int(count)):
-      head = next_head_by_side[side](head)
-      if (must_update(head, tail)):
+      head = next_head[side](head)
+      if (should_move(head, tail)):
         tail = next_tail(head, tail)
         visited.add(''.join(str(e) for e in tail))
-    print('points: ', ' head: ', head, ' tail: ', tail)
 
 print('Part 1: ', visited.__len__())
