3b9c43f269d15c64b68cc06f65289a44e7926e99
Federico Lochbaum <federico.lochbaum@gmail.com>
2022-12-16 07:11:47 -0300

    refactor

--- challenges/Day 15: Beacon Exclusion Zone/beacon _sensors.py
+++ challenges/Day 15: Beacon Exclusion Zone/beacon _sensors.py
@@ -4,8 +4,8 @@ sensors = {} # sensor_point => [becon_point, distance]
 beacons = {} # x: { y: true | Nothinng }
 min_x = float('inf'); max_x = 0
 
-# |y1 - y2| + |x1 - x2|
-def manhattan_distance(p1, p2): return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
+# |x1 - x2| + |y1 - y2|
+def manhattan_distance(p1, p2): return abs(p1[1] - p2[1]) + abs(p1[0] - p2[0])
 
 with open(input_path) as f:
   for line in f:
@@ -40,19 +40,23 @@ with open(input_path) as f:
 
     sensors[''.join([y_sensor_srt[-1], ',', x_sensor_srt[-1]])] = [beacon_point, manhattan_distance(sensor_point, beacon_point)]
 
+def is_invalid_point_for_beacon(point):
+  for sensor in sensors.keys():
+    sensor_point = sensor.split(',')
+    dist = manhattan_distance([int(sensor_point[0]), int(sensor_point[1])], point)
+    if (dist <= sensors[sensor][1] and point != sensors[sensor][0]): return True
+
+  return False
+
 count = 0
 row_to_check = 2000000 # how many positions cannot contain a beacon?
 for c in range(min_x, max_x + 1):
-  point_to_check = [row_to_check, c]
   if (str(row_to_check) in beacons and str(c) in beacons[str(row_to_check)]): continue # There is already a bacon here
   if (''.join([str(row_to_check), ',', str(c)]) in sensors): continue
 
   # Check with the rest of sensors
-  for sensor in sensors.keys():
-    sensor_point = sensor.split(',')
-    dist = manhattan_distance(list(map(int, sensor_point)), point_to_check)
-    if (dist <= sensors[sensor][1]): count += 1; break
-  
+  if (is_invalid_point_for_beacon([row_to_check, c])):  count += 1;
+
 print('Part 1: ', count)
 
 # 4234241 too low
