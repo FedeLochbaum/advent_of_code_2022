bf5aa8a0c252c7e1e0c51471a0990eccf0699629
Federico Lochbaum <federico.lochbaum@gmail.com>
2022-12-17 18:27:43 -0300

    part 1

--- challenges/Day 16: Proboscidea Volcanium/max_flow.py
+++ challenges/Day 16: Proboscidea Volcanium/max_flow.py
@@ -1,4 +1,4 @@
-input_path = 'advent_of_code_2022/challenges/Day 16: Proboscidea Volcanium/input0'
+input_path = 'advent_of_code_2022/challenges/Day 16: Proboscidea Volcanium/input'
 
 initial_time = 30
 initial_valve = 'AA'
@@ -7,32 +7,37 @@ memo = {} # label -> time -> pressure
 rates = {} # label -> flow_rate
 graph = {} # label -> [label]
 count = 0
-initial_state = (initial_valve, initial_time, 0, initial_valve, 0) # Volcano state = (current, missing_time, counter, opens, count_opens)
+initial_state = (initial_valve, initial_time, 0, 0, initial_valve) # Volcano state = (current, missing_time, counter, count_opens, opens)
 
 def possibles_states_from(graph, state):
   next_t = state[1] - 1
   states = []
 
-  if(state[3].find(state[0]) == -1 and rates[state[0]] > 0):
-    states.append((state[0], next_t, state[2] + (rates[state[0]] * next_t), state[3] + ',' + state[0], state[4] + 1))
+  if (next_t == 0): return states
+
+  if(state[4].find(state[0]) == -1 and rates[state[0]] > 0):
+    states.append((state[0], next_t, state[2] + (rates[state[0]] * next_t), state[3] + 1, state[4] + ',' + state[0]))
+
+  if (next_t == 1): return states
 
   for valve in graph[state[0]]:
-    # if (state[3].find(valve) == -1): continue
+    if (state[4].find(valve) != -1): continue #
     states.append((valve, next_t, state[2], state[3], state[4]))
 
   return states
 
 def max_flow(graph, state):
-  if (state[1] == 0 or (state[4] == count)): return state[2] # cutting
+  if (state[1] == 0 or (state[3] == count)): return state[2] # cutting
 
-  if (not state in memo):
-    next_states = possibles_states_from(graph, state)
-    _max = max_flow(graph, next_states[0])
-    for s in next_states[1:]:
-      _max = max(_max, max_flow(graph, s))
-    memo[state] = _max
+  k = (state[0], state[1], state[2]) 
+  if (k not in memo):
+    _max = state[2]
+    possibles = possibles_states_from(graph, state)
+    for s in possibles: _max = max(_max, max_flow(graph, s))
 
-  return memo[state]
+    memo[k] = _max
+
+  return memo[k]
 
 with open(input_path) as f:
   for line in f:
