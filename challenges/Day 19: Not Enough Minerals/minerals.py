c7fc64e6f851454bfd76a79c4dca9b3e32f8e7f0
Federico Lochbaum <federico.lochbaum@gmail.com>
2022-12-22 17:54:37 -0300

    optimization

--- challenges/Day 19: Not Enough Minerals/minerals.py
+++ challenges/Day 19: Not Enough Minerals/minerals.py
@@ -2,67 +2,77 @@ input_path = 'advent_of_code_2022/challenges/Day 19: Not Enough Minerals/input0'
 blueprint = None
 minutes = 24 # hasta 20 puede explorar
 sum = 0
+current_max = 0
 memo = {}
 
 # blueprint = (ore_robot, clay_robot, obsidian_robot, geode_robot)
 # state =
-  # (ore, clay, obsidian) resources
+  # (ore, clay, obsidian, geoda) resources
   # (ore_robot, clay_robot, obsidian_robot, geode_robot) robots
   # current_iteration it
 #
 
-initial_state = lambda: ((0, 0, 0), (1, 0, 0, 0), 0)
-
-def next_resources_to_add(state): return (state[1][0], state[1][1], state[1][2])
+initial_state = lambda: ((0, 0, 0, 0), (1, 0, 0, 0), 0)
 
 def next_states(state):
+  global current_max
   states = []
-  next_turn = state[2] + 1
-  ore_to_increment, clay_to_increment, obsidian_to_icrement = next_resources_to_add(state)
+
+  ore_to_increment, clay_to_increment, obsidian_to_icrement, geodas_to_increment = state[1]
+
+  next_geodas = state[0][3] + geodas_to_increment
 
   # priorizing build geode robot
-  c_geodes = int(min( state[0][0] / blueprint[3][0], state[0][2] / blueprint[3][1] ))
+  c_geodes = min( state[0][0] // blueprint[3][0], state[0][2] // blueprint[3][1] )
+  ore = state[0][0] - (c_geodes * blueprint[3][0])
+  obsidian = state[0][2] - (c_geodes * blueprint[3][1])
 
   # priorizing build obsidian robots
-  ore = state[0][0] - (c_geodes * blueprint[3][0])
-
-  count_obsidian = int(min(ore / blueprint[2][0], state[0][1] / blueprint[2][1]))
-
+  count_obsidian = min(ore // blueprint[2][0], state[0][1] // blueprint[2][1])
   ore = ore - (count_obsidian * blueprint[2][0])
-  next_clay = state[0][1] - (count_obsidian * blueprint[2][1]) + clay_to_increment
-  next_obsidian = state[0][2] - (c_geodes * blueprint[3][1]) + obsidian_to_icrement
+  clay = state[0][1] - (count_obsidian * blueprint[2][1])
 
-  max_count_ore = int(ore / blueprint[0])
-  max_count_clay = int(ore / blueprint[1])
+  next_clay = clay + clay_to_increment
+  next_obsidian = obsidian + obsidian_to_icrement
 
-  for count_ore in range(0, max_count_ore + 1):
-    for count_clay in range(0, max_count_clay +1):
-      if ((count_ore * blueprint[0] + (count_clay * blueprint[1])) > ore): continue
-      
-      _resources = (
-        (ore - (count_ore * blueprint[0]) - (count_clay * blueprint[1])) + ore_to_increment,
-        next_clay,
-        next_obsidian
-      )
+  max_count_clay = ore // blueprint[1]
 
-      states.append((
-        _resources,
-        (state[1][0] + count_ore, state[1][1] + count_clay, state[1][2] + count_obsidian, state[1][3] + c_geodes),
-        next_turn
-      ))
+  for count_clay in range(0, max_count_clay + 1):
+    partial_ore = ore - (count_clay * blueprint[1])
+    count_ore = partial_ore // blueprint[0]
 
-  return states[:2]
+    partial_ore = partial_ore - (count_ore * blueprint[0])
+
+    _resources = (
+      partial_ore + ore_to_increment,
+      next_clay,
+      next_obsidian,
+      next_geodas
+    )
+
+    _robots = (
+      state[1][0] + count_ore,
+      state[1][1] + count_clay,
+      state[1][2] + count_obsidian,
+      state[1][3] + c_geodes
+    )
+
+    new_state = (_resources, _robots, state[2] + 1)
+    states.append(new_state)
+
+  return states
 
 def simulate(state):
-  if (state[2] == minutes): return 0
+  global current_max
+  if (state[2] == minutes): return state[0][3]
   if (state not in memo):
     _max = 0
-    for s in next_states(state):
-      _max = max(_max, simulate(s))
-    memo[state] = state[1][3] + _max
+    for s in next_states(state): _max = max(_max, simulate(s))
+    current_max = max(current_max, _max)
+    memo[state] = _max
   return memo[state]
 
-def simulate_blueprint(): global memo; memo = {}; return simulate(initial_state())
+def simulate_blueprint(): global memo; global current_max; current_max = 0; memo = {}; return simulate(initial_state())
 
 with open(input_path) as f:
   index = 1
