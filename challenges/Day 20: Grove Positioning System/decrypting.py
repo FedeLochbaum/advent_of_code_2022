b0d52c323e90416395acab18f8f38c53bb51e779
Federico Lochbaum <federico.lochbaum@gmail.com>
2022-12-24 05:16:33 -0300

    day 20

--- challenges/Day 20: Grove Positioning System/decrypting.py
+++ challenges/Day 20: Grove Positioning System/decrypting.py
@@ -1,14 +1,17 @@
 input_path = 'advent_of_code_2022/challenges/Day 20: Grove Positioning System/input'
-
 array = []
 numbers = {}
-L = 5000
+decryption_key = 811589153
+times = 10
+L = 0
 
 with open(input_path) as f:
   for line in f:
-    n = int(line[:-1])
-    numbers[n] = (array.__len__(), 0)
+    n = int(line[:-1]) * decryption_key
+    numbers[n] = (len(array), 0)
     array.append(n)
+
+L = len(array)
 
 def shift_to(array, _from, dir):
   temp = array[_from]
@@ -20,30 +23,30 @@ def shift_to(array, _from, dir):
   return next_pos
 
 def get_move(pos, n):
-  n = (abs(n) % L) * (-1 if n < 0 else 1) # TODO: REVIEW THIS
+  n = (abs(n) % L) * (-1 if n < 0 else 1)
   # <-
-  if (n < 0): return [( L - ( abs(n) - pos ) ) - pos - 1, 1] if (pos + n) <= 0 else [n, -1]
+  if (n < 0): return [( L - (( abs(n) - pos ) ) - pos) - 1, 1] if (pos + n) < 0 else [n, -1]
 
   # ->
-  return [pos - ( n - ( L - pos ) ) - 1, -1] if (pos + n) >= L else [n, 1]
+  return [pos - ( n - ( L - pos ) ) - 1, -1] if (pos + n) > L else [n, 1]
 
 def shift_number(array, _from, N):
   count, dir = get_move(_from, N)
   for _ in range(count): _from = shift_to(array, _from, dir)
 
-def mix_numbers():
-  copy = array.copy()
-  for i in range(len(array)):
-    cur_pos = (numbers[array[i]][0] +  numbers[array[i]][1]) % len(array)
-    shift_number(copy, cur_pos, array[i])
+def mix_numbers(original):
+  copy = original.copy()
+  for i in range(L):
+    cur_pos = (numbers[original[i]][0] + numbers[original[i]][1]) % L
+    shift_number(copy, cur_pos, original[i])
   return copy
 
-L = len(array)
-new_array = mix_numbers()
+def print_result(a, t):
+  i = a.index(0); ns = [a[(i + 1000) % L], a[(i + 2000) % L], a[(i + 3000) % L]]
+  print('Part ', t, ': ',  sum(ns))
 
-print('new array: ', new_array)
-i = new_array.index(0)
+for i in range(times):
+  array = mix_numbers(array)
+  if (i == 0): print_result(array, 1)
 
-ns = [new_array[(i + 1000) % L], new_array[(i + 2000) % L], new_array[(i + 3000) % L]]
-
-print('Part 1: ',  sum(ns))
+print_result(array, 2)
