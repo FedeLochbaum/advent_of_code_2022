ab8ece324e64d9d9897abf8e21b7e96e9a15e23b
Federico Lochbaum <federico.lochbaum@gmail.com>
2022-12-24 19:32:14 -0300

    day 22

--- challenges/Day 22: Monkey Map/force_field.py
+++ challenges/Day 22: Monkey Map/force_field.py
@@ -1,4 +1,4 @@
-input_path = 'advent_of_code_2022/challenges/Day 22: Monkey Map/input0'
+input_path = 'advent_of_code_2022/challenges/Day 22: Monkey Map/input'
 
 RIGHT = 0; DOWN = 1; LEFT = 2; UP = 3
 facing = RIGHT
@@ -6,6 +6,7 @@ pos = None
 done = False
 instructions = []
 screen = {} # row index -> (column shifting, row)
+L = 0 # Counts of rows
 
 next_face = {
   RIGHT: { 'R': DOWN, 'L': UP },
@@ -15,14 +16,12 @@ next_face = {
 }
 
 next_to_sum = { UP: (-1, 0), DOWN: (1, 0), RIGHT: (0, 1), LEFT: (0, -1) }
-
 def next_pos_from(_pos): r, c = next_to_sum[facing]; return _pos[0] + r, _pos[1] + c
-
 is_in_range = lambda i, row: (i >= 0 and i < len(row))
 
 def first_bottom_row(r, c):
-  for _r in range(r + 1, len(screen.keys())):
-    if _r == len(screen.keys()) - 1: return _r
+  for _r in range(r + 1, L):
+    if _r == L - 1: return _r
     
     shift, row = screen[_r]
     if (not is_in_range(c - shift, row)): return _r - 1
@@ -34,30 +33,26 @@ def first_top_row(r, c):
     shift, row = screen[_r]
     if (not is_in_range(c - shift, row)): return _r + 1
 
-
 def next_pos_wrapping(r, c):
-  shift, row = screen[r]
-
-  if facing == RIGHT: return (r, shift)
-  if facing == LEFT: return (r, len(row) + shift)
-
   if (facing == UP): return (first_bottom_row(r, c), c)
   if (facing == DOWN): return (first_top_row(r, c), c)
+
+  shift, row = screen[r]
+  if facing == RIGHT: return (r, shift)
+  if facing == LEFT: return (r, len(row) -1 + shift)
 
 def turn_to(dir): global facing; facing = next_face[facing][dir]
 def move_forward(count, pos):
   _pos = pos
   for _ in range(count):
     r, c = next_pos_from(_pos)
+
+    if ((r < 0 or r >= L) or not (is_in_range(c - screen[r][0], screen[r][1]))): r, c = next_pos_wrapping(r, c)
+
     shift, row = screen[r]
+    if (row[c - shift] == '#'): break
 
-    valid_pos = (r, c) if is_in_range(c - shift, row) else next_pos_wrapping(r, c)
-
-    shift, row = screen[valid_pos[0]]
-
-    if (row[valid_pos[1] - shift] == '#'): break
-
-    _pos = valid_pos
+    _pos = r, c
 
   return _pos
 
@@ -88,9 +83,13 @@ with open(input_path) as f:
     screen[row] = (first_col, list(line[first_col:-1]))
     row += 1
 
+L = len(screen)
+
+i_i = 0
 for inst in instructions:
   if (type(inst) == int):
     pos = move_forward(inst, pos)
   else: turn_to(inst)
+  i_i += 1
 
 print('Part 1: ', 1000 * (pos[0] + 1) + 4 * (pos[1] + 1) + facing)
