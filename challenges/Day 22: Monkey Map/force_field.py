f0bdc8e21c0b53e9b48194cf51de2bc2aa3b1df3
Federico Lochbaum <federico.lochbaum@gmail.com>
2022-12-25 17:19:54 -0300

    day 22

--- challenges/Day 22: Monkey Map/force_field.py
+++ challenges/Day 22: Monkey Map/force_field.py
@@ -5,8 +5,20 @@ facing = RIGHT
 pos = None
 done = False
 instructions = []
-screen = {} # row index -> (column shifting, row)
+screen = {} # row index -> (column_shifting, row)
 L = 0 # Counts of rows
+
+REGIONS = { # each region is 50 x 50
+                 'B': (0, 50), 'C': (0, 100),
+                 'A': (50, 50),
+  'E': (100, 0), 'D': (100, 50),
+  'F': (150, 0)
+}
+
+def region_by_point(_pos):
+  for region in REGIONS.keys():
+    if _pos[0] >= REGIONS[region][0] and _pos[1] >= REGIONS[region][1] and _pos[0] < REGIONS[region][0] + 50 and _pos[1] < REGIONS[region][1] + 50:
+      return region
 
 next_face = {
   RIGHT: { 'R': DOWN,  'L': UP },
@@ -19,35 +31,52 @@ next_to_sum = { UP: (-1, 0), DOWN: (1, 0), RIGHT: (0, 1), LEFT: (0, -1) }
 def next_pos_from(_pos): r, c = next_to_sum[facing]; return (_pos[0] + r, _pos[1] + c)
 is_in_range = lambda i, row: i >= 0 and i < len(row)
 
-def first_bottom_row(r, c):
-  for _r in range(r + 1, L):
-    if _r == L - 1: return _r
+wrapping = {
+  'A': {
+    RIGHT: lambda r, _: (REGIONS['C'][0] + 49, REGIONS['C'][1] + r, UP),
+    LEFT: lambda r, _: (REGIONS['E'][0], REGIONS['E'][1] + r, DOWN),
+  },
+  'B': {
+    UP: lambda _, c: (REGIONS['F'][0] + c, REGIONS['F'][1] + 0, RIGHT),
+    LEFT: lambda _, c: (REGIONS['E'][0] + c, REGIONS['E'][1], RIGHT),
+  },
+  'C': {
+    UP: lambda _, c: (REGIONS['F'][0] + 49, REGIONS['F'][1] + c, UP),
+    DOWN: lambda _, c: (REGIONS['A'][0] + c, REGIONS['A'][1] + 49, LEFT),
+    RIGHT: lambda _, c: (REGIONS['D'][0] + c, REGIONS['D'][1] + 49, LEFT),
+  },
+  'D': {
+    DOWN: lambda _, c: (REGIONS['F'][0] + c, REGIONS['F'][1] + 49, LEFT),
+    RIGHT: lambda r, _: (REGIONS['C'][0] + 49 - r, REGIONS['C'][1] + 49, LEFT),
+  },
+  'E': {
+    UP: lambda _, c: (REGIONS['A'][0] + c, REGIONS['A'][1], RIGHT),
+    LEFT: lambda r, _: (REGIONS['B'][0] + 49 - r , REGIONS['B'][1], RIGHT),
+  },
+  'F': {
+    DOWN: lambda _, c: (REGIONS['C'][0], REGIONS['C'][1] + c, DOWN),
+    RIGHT: lambda r, _: (REGIONS['D'][0] + 49, REGIONS['D'][1] + r, UP),
+    LEFT: lambda r, c: (REGIONS['B'][0], REGIONS['B'][1] + c, DOWN),
+  },
+}
 
-    shift, row = screen[_r]
-    if (not is_in_range(c - shift, row)): return _r - 1
-
-def first_top_row(r, c):
-  for _r in reversed(range(0, r - 1)):
-    if _r == 0: return _r
-
-    shift, row = screen[_r]
-    if (not is_in_range(c - shift, row)): return _r + 1
-
-def next_pos_wrapping(r, c):
-  if (facing == UP): return first_bottom_row(r, c), c
-  if (facing == DOWN): return first_top_row(r, c), c
-
-  shift, row = screen[r]
-  if facing == RIGHT: return r, shift
-  if facing == LEFT: return r, shift + (len(row) - 1)
-
+def next_pos_wrapping(pos):
+  global facing
+  shift, _ = screen[pos[0]]
+  region = region_by_point(pos)
+  _r = pos[0] - REGIONS[region][0]
+  _c = pos[1] - REGIONS[region][1]
+  r, c, f = wrapping[region][facing](_r, _c); facing = f; return (r, c)
 def turn_to(dir): global facing; facing = next_face[facing][dir]
 def move_forward(count, pos):
+  global facing
   _pos = pos
   for _ in range(count):
     r, c = next_pos_from(_pos)
-    if ((r < 0 or r >= L) or not (is_in_range(c - screen[r][0], screen[r][1]))): r, c = next_pos_wrapping(r, c)
-    if (screen[r][1][c - screen[r][0]] == '#'): break
+    _facing = facing
+    if ((r < 0 or r >= L) or not (is_in_range(c - screen[r][0], screen[r][1]))): r, c = next_pos_wrapping(_pos)
+
+    if (screen[r][1][c - screen[r][0]] == '#'): facing = _facing; break # rollback facing
     _pos = r, c
   return _pos
 
@@ -83,5 +112,4 @@ for inst in instructions:
   if (type(inst) == int): pos = move_forward(inst, pos)
   else: turn_to(inst)
 
-print('Part 1: ', 1000 * (pos[0] + 1) + 4 * (pos[1] + 1) + facing)
-# 47462
+print('Part 2: ', 1000 * (pos[0] + 1) + 4 * (pos[1] + 1) + facing)
